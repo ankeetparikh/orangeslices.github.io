f6098576326d75cd4e586efda77ec4488c05fa79
Ankeet <ankeet.parikh96@gmail.com>
2014-10-11 23:24:06 -0400

    More stuff Added

--- TWG.js
+++ TWG.js
@@ -1,15 +1,22 @@
 var w1 = document.getElementById('w1');
-var w2 = document.getElementById('w2');
+var w3 = document.getElementById('w3');
+
 var Question = function(first, second, third)
 {
 	this.first = first;
 	this.answer = second; //answer to the question
-	this.last = third;
+	this.third = third;
 }
 
 var questionList = new Array(); //Array of Questions
+
+questionList.push(new Question('bengal','tiger', 'shark'));
 
 var nextQuestion = function()
 {
 	return questionList[Math.floor(Math.random()*questionList.length)];
 }
+
+w1.innerHTML = questionList[0].first;
+w3.innerHTML = questionList[0].third;
+console.log(questionList[0].third);
