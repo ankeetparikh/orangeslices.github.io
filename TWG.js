1104e86455561795e0b1e9863d009e4168153f1b
Ankeet <ankeet.parikh96@gmail.com>
2014-10-11 23:18:14 -0400

    Continued Programming

--- TWG.js
+++ TWG.js
@@ -1,2 +1,15 @@
-var para = document.getElementById("paragraph");
-			para.innerHTML = "Hello World";
+var w1 = document.getElementById('w1');
+var w2 = document.getElementById('w2');
+var Question = function(first, second, third)
+{
+	this.first = first;
+	this.answer = second; //answer to the question
+	this.last = third;
+}
+
+var questionList = new Array(); //Array of Questions
+
+var nextQuestion = function()
+{
+	return questionList[Math.floor(Math.random()*questionList.length)];
+}
